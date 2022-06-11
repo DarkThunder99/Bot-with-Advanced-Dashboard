7c8b3338f2c6da58475b229ff9249abf3b379ad8
Imhereforadiscordbot <101909226+Imhereforadiscordbot@users.noreply.github.com>
2022-06-11 16:46:06 -0600

    Update and rename MEE8.js to Pogy.js

--- Pogy.js
+++ Pogy.js
@@ -6,7 +6,7 @@ const { token } = require('./utils/variables');
 
 
 
-module.exports = class MEE8Client extends Client {
+module.exports = class PogyClient extends Client {
 	constructor(options = {}, sentry) {
 	  super({
       partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
